de8ac78db49056e1ba0841286e0b82f9bc033f92
Madjid <madjid.bouchair@gmail.com>
2017-01-30 16:14:31 +0100

    Server

--- ResTest.py
+++ ResTest.py
@@ -1,5 +1,14 @@
+import os
 from eve import Eve
+
+if 'PORT' in os.environ:
+    port = int(os.environ.get('PORT'))
+    host = '0.0.0.0'
+else:
+    port = 5000
+    host = '127.0.0.1'
+
 app = Eve()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host=host, port= port,debug=True)
